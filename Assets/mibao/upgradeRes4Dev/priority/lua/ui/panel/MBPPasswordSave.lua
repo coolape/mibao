f8296a77aa8d97b17ed67c1a6972c24942606102
chenbin <chbin12@126.com>
2017-12-06 19:48:02 +0800

    no message

--- Assets/mibao/upgradeRes4Dev/priority/lua/ui/panel/MBPPasswordSave.lua
+++ Assets/mibao/upgradeRes4Dev/priority/lua/ui/panel/MBPPasswordSave.lua
@@ -5,6 +5,7 @@ do
     local csSelf = nil;
     local transform = nil;
     local objs = {}
+    local isShowingSearch = false;
 
     -- 初始化，只会调用一次
     function MBPPasswordSave.init(csObj)
@@ -15,6 +16,9 @@ do
         --]]
         ---@type Coolape.CLUILoopGrid
         objs.grid = getCC(transform, "PanelList/Grid", "CLUILoopGrid")
+        ---@type TweenPosition
+        objs.search = getCC(transform, "AnchorTop/search", "TweenPosition")
+        objs.InputSearchKey = getCC(objs.search.transform, "InputSearchKey", "UIInput")
     end
 
     -- 设置数据
@@ -23,6 +27,8 @@ do
 
     -- 显示，在c#中。show为调用refresh，show和refresh的区别在于，当页面已经显示了的情况，当页面再次出现在最上层时，只会调用refresh
     function MBPPasswordSave.show()
+        isShowingSearch = false;
+        objs.search:ResetToBeginning();
     end
 
     function MBPPasswordSave.initCell(cell, data)
@@ -62,8 +68,28 @@ do
         elseif (goName == "ButtonAdd") then
             getPanelAsy("PanelPasswordSaveEditor", onLoadedPanelTT, nil)
         elseif goName == "ButtonSearch" then
-
+            isShowingSearch = not isShowingSearch;
+            objs.search:Play(isShowingSearch)
+        elseif goName == "InputSearchKey" then
+            objs.InputSearchKey.value = trim(objs.InputSearchKey.value)
+            if isNilOrEmpty( objs.InputSearchKey.value) then
+                objs.grid:setList(MBDBPassword.getData(), MBPPasswordSave.initCell);
+                return
+            end
+            MBPPasswordSave.search(objs.InputSearchKey.value)
         end
+    end
+
+    function MBPPasswordSave.search(key)
+        local ret = {}
+        local list = MBDBPassword.getData();
+        for i = 0, list.Count - 1 do
+            if string.find(MapEx.getString(list[i], "platform"), key) or string.find(MapEx.getString(list[i], "desc"), key) then
+                table.insert(ret, list[i])
+            end
+        end
+
+        objs.grid:setList(ret, MBPPasswordSave.initCell);
     end
 
     -- 当按了返回键时，关闭自己（返值为true时关闭）
