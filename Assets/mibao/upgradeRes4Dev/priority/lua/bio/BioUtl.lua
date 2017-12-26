48cf78eaee83805cecc9e29439362ff4a55769d6
chenbin <chbin12@126.com>
2017-12-26 22:27:29 +0800

    ===

--- Assets/mibao/upgradeRes4Dev/priority/lua/bio/BioUtl.lua
+++ Assets/mibao/upgradeRes4Dev/priority/lua/bio/BioUtl.lua
@@ -6,19 +6,27 @@ do
 
     function BioUtl.writeObject(obj)
         local os = LuaB2OutputStream.new();
-        BioOutputStream.writeObject(os, obj);
-        local bytes = os:toBytes();
-        os:release();
-        os = nil;
-        return bytes;
+        local status = pcall(BioOutputStream.writeObject, os, obj);
+        if status then
+            local bytes = os:toBytes();
+            os:release();
+            os = nil;
+            return bytes;
+        else
+            return nil;
+        end
     end
 
     function BioUtl.readObject(bytes)
         local is = LuaB2InputStream.new(bytes);
-        local result = BioInputStream.readObject(is);
-        is:release();
-        is = nil;
-        return result;
+        local status, result = pcall(BioInputStream.readObject, is);
+        if status then
+            is:release();
+            is = nil;
+            return result;
+        else
+            return nil;
+        end
     end
     --------------------------------------------
     return BioUtl;
